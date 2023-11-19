99f2ebe7d6e554828afa1d75b81f8d7ebde5dc1a
BEKKIE <iambekkie99@gmail.com>
2023-11-19 17:34:00 +0500

    fixing errors

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,21 +1,9 @@
-import { defineConfig } from 'vite'
+import react from 'vite-plugin-react';
+import { defineConfig } from 'vite';
 
-
-import react from '@vitejs/plugin-react-swc'
-
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    proxy: {
-      '/api': {
-        target: 'https://deployment-server-kappa.vercel.app',
-        secure: false,
-      },
-    },
+    proxy: 'https://deployment-server-kappa.vercel.app',
   },
   plugins: [react()],
-  
-  
-}
-)
-
+});
