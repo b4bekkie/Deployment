62def4a03aad5e5506518a5ff19c52af1c056dbc
BEKKIE <iambekkie99@gmail.com>
2023-11-19 17:42:25 +0500

    ss

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,17 +1,20 @@
-import { defineConfig } from 'vite'
 
 
-import react from '@vitejs/plugin-react-swc'
+// import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-
-  server: {
-    //  proxy: 'https://deployment-server-kappa.vercel.app',
-  },
-  plugins: [react()],
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   // server: {
+//   //   proxy: {
+//   //     '/api': {
+//   //       target: 'https://bekkie-etstate.vercel.app',
+//   //       secure: false,
+//   //     },
+//   //   },
+//   // },
+//   plugins: [react()],
   
   
-}
-)
+//  }
+// )
 
