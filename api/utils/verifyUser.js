154f87597a503bd50d7b072a849ccbbb3af1086d
BEKKIE <iambekkie99@gmail.com>
2023-11-19 20:07:03 +0500

    fixing jwt token issue

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -2,14 +2,14 @@
 import { errorHandler } from './error.js';
 import jwt from 'jsonwebtoken';
 
-const JWT_SECRET = 'abcdefghi';
+
 
 export const verifyToken = (req, res, next) => {
   const token = req.cookies.access_token;
 
   if (!token) return next(errorHandler(401, 'Unauthorized'));
 
-  jwt.verify(token, JWT_SECRET, (err, user) => {
+  jwt.verify(token, process.env.JWT_SECRET || 'ro8BS6Hiivgzy8Xuu09JDjlNLnSLldY5', (err, user) => {
     if (err) return next(errorHandler(403, 'Forbidden'));
 
     req.user = user;
