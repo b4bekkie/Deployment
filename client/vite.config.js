778f7b97fdd61dbc7cf4ce87306e1ca6ae920291
BEKKIE <iambekkie99@gmail.com>
2023-11-19 18:00:34 +0500

    fixing errros

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,16 @@
-import { defineConfig } from 'vite'
+
 
 
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
   server: {
-    proxy: {
-      '/api': {
+    // proxy: {
+    //   '/api': {
        
-      },
-    },
+    //   },
+    // },
   },
   plugins: [react()],
   
