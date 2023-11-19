235bde8e167d444d165484259b35904ab015bd85
BEKKIE <iambekkie99@gmail.com>
2023-11-19 20:13:17 +0500

    again checking

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -2,8 +2,7 @@ import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs'
 import { errorHandler } from "../utils/error.js";
 
-import jwt from "jsonwebtoken";
-
+import Jwt from "jsonwebtoken";
 
 
 
@@ -35,7 +34,7 @@ export const signin = async (req, res, next) => {
       if (!validUser) return next(errorHandler(404, 'User not found!'));
       const validPassword = bcryptjs.compareSync(password, validUser.password);
       if (!validPassword) return next(errorHandler(401, 'Wrong credentials!'));
-      const token =  jwt.sign({ id: validUser._id }, process.env.JWT_SECRET || 'ro8BS6Hiivgzy8Xuu09JDjlNLnSLldY5',);
+      const token =  Jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
       const { password: pass, ...rest } = validUser._doc;
       res
         .cookie('access_token', token, { httpOnly: true })
@@ -54,7 +53,7 @@ export const google = async (req, res, next) => {
 
    if (user) {
 
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET || 'ro8BS6Hiivgzy8Xuu09JDjlNLnSLldY5',);
+    const token = Jwt.sign({ id: user._id }, process.env.JWT_SECRET);
 
      const { password: pass, ...rest } = user._doc;
 
@@ -79,7 +78,7 @@ export const google = async (req, res, next) => {
 
       await newUser.save();
 
-      const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET) || 'ro8BS6Hiivgzy8Xuu09JDjlNLnSLldY5';
+      const token = Jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
 
       const { password: pass, ...rest } = newUser._doc;
 
