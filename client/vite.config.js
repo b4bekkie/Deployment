cac7c020aaaaf7e9bf4369ae8aaaaf43b60d2f93
BEKKIE <iambekkie99@gmail.com>
2023-11-19 17:35:55 +0500

    fixedd

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,17 @@
-import react from 'vite-plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
 
+
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
 export default defineConfig({
+
   server: {
     proxy: 'https://deployment-server-kappa.vercel.app',
   },
   plugins: [react()],
-});
+  
+  
+}
+)
+
