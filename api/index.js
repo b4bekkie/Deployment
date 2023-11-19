317c7a32b2a5608418122165633eeea1b9250cea
BEKKIE <iambekkie99@gmail.com>
2023-11-19 17:08:12 +0500

    first commit

---
+++ api/index.js
@@ -0,0 +1,57 @@
+import express from 'express'
+
+const app = express();
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+import userRouter from './routes/user.route.js'
+import authRouter from './routes/auth.route.js'
+import listingRouter from './routes/listing.route.js';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+dotenv.config()
+
+
+mongoose.connect(process.env.MONGOSE)
+.then( ()=>{
+    console.log('Connected to DataBase')
+})
+.catch((e)=>{
+ console.log(e)
+})
+
+
+  
+
+
+app.use(express.json())
+app.use(cookieParser());
+
+app.use(cors({
+  origin: 'https://bekkie-etstate-wnqa.vercel.app', // Replace with your frontend URL
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true, // Allows cookies to be sent from the frontend
+}));
+
+
+
+ 
+
+
+app.listen(3000,()=>{
+    console.log('app is running on port 3000') 
+})
+
+ 
+app.use('/api/user',userRouter)
+app.use('/api/auth',authRouter)
+app.use('/api/listing', listingRouter);
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+      success: false,
+      statusCode,
+      message,
+    });
+  });
